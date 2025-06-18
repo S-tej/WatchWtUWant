cbba35744ede8717a4f72e89e401a20b4dd876df
S-tej <swarantejvajrapu8055@gmail.com>
2025-06-19 00:47:38 +0530

    Utils been updated, cors and cookieparser introduced

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 // require('dotenv').config({path:'./env'}) // Can use this but this will look wierd for the usage
 import connectDB from "./db/index.js"
 import dotenv from "dotenv"
+import app from "./app.js"
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error :",error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`MongoDB connected sucessfully on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed ! the error is : ",error)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) => (req,res,next) => {  // function of function i.e ()=>{()=>{}} === ()=>()=>{}
+    Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err)) // This is promise syntax
+}
+
+export { asyncHandler }
+
+// This try - catch
+
+// const asyncHandler = (fn) => async () => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message 
+//         })
+//     }
+// }
+
+// export {asyncHandler}
